e33d651e9776c1312bca4ea96dcccdacb4eeb860
r7kamura <r7kamura@gmail.com>
2018-11-02 06:29:14 +0900

    Add invalid case for Rnes::InesHeader#valid?

--- spec/rnes/ines_header_spec.rb
+++ spec/rnes/ines_header_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe Rnes::InesHeader do
       ines_header.valid?
     end
 
+    context 'with invalid bytes' do
+      let(:data) do
+        []
+      end
+
+      it 'returns false' do
+        is_expected.to eq(false)
+      end
+    end
+
     context 'with leading NES + 0x1A bytes' do
       let(:data) do
         'NES'.chars.map(&:ord) + [0x1A]
