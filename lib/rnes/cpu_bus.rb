1cf635133247a900aaa0ff6208d4a0ba63ce3538
r7kamura <r7kamura@gmail.com>
2018-11-04 23:14:50 +0900

    Raise error on invalid address

--- lib/rnes/cpu_bus.rb
+++ lib/rnes/cpu_bus.rb
@@ -25,12 +25,12 @@ module Rnes
         @ppu.read(address - 0x2000)
       when 0x2008..0x3FFF
         @ppu.read(address - 0x2008)
-      when 0x4000..0x401F
-        0 # TODO
-      when 0x4020..0x5FFF
-        # TODO
-      when 0x6000..0x7FFF
-        # TODO
+      # when 0x4000..0x401F
+      #   0 # TODO
+      # when 0x4020..0x5FFF
+      #   0 # TODO
+      # when 0x6000..0x7FFF
+      #   0 # TODO
       when 0x8000..0xBFFF
         try_to_read_program_rom(address - 0x8000)
       when 0xC000..0xFFFF
@@ -53,12 +53,12 @@ module Rnes
         @ppu.write(address - 0x2000, value)
       when 0x2008..0x3FFF
         @ppu.write(address - 0x2008, value)
-      when 0x4000..0x401F
-        # TODO
-      when 0x4020..0x5FFF
-        # TODO
-      when 0x6000..0x7FFF
-        # TODO
+      # when 0x4000..0x401F
+      #   # TODO
+      # when 0x4020..0x5FFF
+      #   # TODO
+      # when 0x6000..0x7FFF
+      #   # TODO
       else
         raise ::Rnes::Errors::InvalidCpuBusAddressError, address
       end
